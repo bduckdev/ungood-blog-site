7e71c427422de51daa1c57e05ec510a934e56c93
Drew Pereli <drewpereli@gmail.com>
2024-03-15 10:01:30 -0700

    Setup prisma. Must create .env file, see .env.example

---
+++ prisma/migrations/20240315155418_add_users/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `authorId` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "authorId" INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
